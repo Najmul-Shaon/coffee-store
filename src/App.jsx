fbae22ffc11bcecc852f876fdeb4d6f72bbac049
Najmul shaon <najmul.nh.shaon@gmail.com>
2024-11-29 00:26:46 +0600

    delete state update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { useLoaderData } from "react-router-dom";
 import CoffeeCard from "./Components/CoffeeCard";
+import { useState } from "react";
 
 function App() {
-  const coffees = useLoaderData();
-
+  const loadedCoffees = useLoaderData();
+  const [coffees, setCoffees] = useState(loadedCoffees);
   return (
     <>
       <h1 className="text-3xl text-red-400 btn">
         Total Coffees : {coffees.length}
       </h1>
       {coffees.map((coffee) => (
-        <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>
+        <CoffeeCard
+          key={coffee._id}
+          coffee={coffee}
+          coffees={coffees}
+          setCoffees={setCoffees}
+        ></CoffeeCard>
       ))}
     </>
   );
