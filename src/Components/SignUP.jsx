f0ded05139d14e75127da8a6f3e70fb97d2d3959
Najmul shaon <najmul.nh.shaon@gmail.com>
2024-11-30 00:32:11 +0600

    user delete

--- src/Components/SignUP.jsx
+++ src/Components/SignUP.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
+import Swal from "sweetalert2";
 
 const SignUP = () => {
   const { createUser } = useContext(AuthContext);
@@ -7,12 +8,38 @@ const SignUP = () => {
   const handleSignUp = (e) => {
     e.preventDefault();
     const form = e.target;
+    const name = form.name.value;
     const email = form.email.value;
     const pass = form.password.value;
     console.log({ email, pass });
     createUser(email, pass)
       .then((res) => {
         console.log(res.user);
+
+        const createAt = res.user.metadata.creationTime;
+
+        const newUser = { name, email, createAt };
+
+        //   save new user info to db
+        fetch("http://localhost:5000/users", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newUser),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.insertedId) {
+              Swal.fire({
+                title: "success!",
+                text: "Coffee added",
+                icon: "success",
+                confirmButtonText: "Cool",
+              });
+            }
+          });
       })
       .catch((e) => {
         console.log(e.code);
@@ -23,6 +50,18 @@ const SignUP = () => {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <form onSubmit={handleSignUp} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                className="input input-bordered"
+                required
+              />
+            </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import UpdateCoffee from "./Components/UpdateCoffee.jsx";
 import SIgnIn from "./Components/SIgnIn.jsx";
 import SignUP from "./Components/SignUP.jsx";
 import AuthProvider from "./Providers/AuthProvider.jsx";
+import Users from "./Components/Users.jsx";
 
 const router = createBrowserRouter([
   {
@@ -32,6 +33,11 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignUP></SignUP>,
+  },
+  {
+    path: "/users",
+    element: <Users></Users>,
+    loader: () => fetch("http://localhost:5000/users"),
   },
 ]);
 
